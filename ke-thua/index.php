436b7a0a9e0cf82323bfe64edb7a84764b2e9a52
thienth32 <thienth32@gmail.com>
2020-09-18 12:31:13 +0700

    Update index.php

--- ke-thua/index.php
+++ ke-thua/index.php
@@ -3,18 +3,37 @@ require_once './Product.php';
 require_once './User.php';
 require_once './Category.php';
 
-$productModel = new Product();
-// $products = $productModel->all();
+// $productModel = new Product();
+// // $products = $productModel->all();
 
-// $userModel = new User();
-// $users = $userModel->all();
+// // $userModel = new User();
+// // $users = $userModel->all();
 
-$cateModel = new Category();
-$cates = $productModel->findOne(93);
+// $cateModel = new Category();
+// $cates = $productModel->findOne(93);
 
-echo "<pre>";
+// echo "<pre>";
 
-var_dump($cates);
+// var_dump($cates);
+/*
+1. ở trang index.php lấy ra danh sách tất cả các sản phẩm có trong db
+hiển thị dạng table
+mỗi dòng của table là 1 bản ghi trong db
+2. cuối mỗi dòng thì sẽ có 1 đường link để xóa 
+=> xoa.php?id=id của bản ghi
+
+tại trang xoa.php thực hiện xóa sản phẩm dựa vào id sau đó 
+điều hướng website về trang index
+
+3. bên cạnh đường dẫn xóa thì sẽ có đường dẫn chi tiết
+=> chi-tiet.php?id=id của bản ghi
+
+tại trang chi tiết lấy ra thông tin chi tiết của sản phẩm theo id
+
+tg làm bài: 12h30 -> 1h
+
+
+*/
 
 
 ?>

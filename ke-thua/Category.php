608742a855970bc1fd15694ff1afe450b5c4c0cd
thienth32 <thienth32@gmail.com>
2020-09-18 13:42:58 +0700

    update

--- ke-thua/Category.php
+++ ke-thua/Category.php
@@ -2,6 +2,12 @@
 require_once './BaseModel.php';
 class Category extends BaseModel{
     var $table = 'categories';
+
+    function countProducts(){
+        $query = "select * from products where cate_id = " . $this->id;
+        $totalProducts = $this->executeQuery($query); 
+        return count($totalProducts);
+    }
 }
 
 ?>
---
+++ ke-thua/danh-muc.php
@@ -0,0 +1,33 @@
+<?php
+require_once './Category.php';
+
+$cateModel = new Category();
+$cates = $cateModel->all();
+/*
+    Hiển thị danh sách danh mục trong database
+    id      Cate Name       Number Product
+    
+    (Number Product là số lượng sản phẩm thuộc
+    danh mục đang hiển thị)
+
+*/
+?>
+<table>
+    <thead>
+    <th>ID</th>
+    <th>Cate Name</th>
+    <th>Number product</th>
+    <th>
+        <a href="">Add</a>
+    </th>
+    </thead>
+    <tbody>
+        <?php foreach ($cates as $cate): ?>
+            <tr>
+            <td><?= $cate->id ?></td>
+            <td><?= $cate->cate_name ?></td>
+            <td><?= $cate->countProducts() ?></td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
